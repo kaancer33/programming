cb29036b2e27c7f6e5a06b34df0c3f7bb0f3a828
kaanceran <49731056+kaancer33@users.noreply.github.com>
2022-10-05 14:52:16 +0300

    cpp files

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+/*
+ * This is the main function for the program.
+*/
+
+int a = 3215;
+
+void helloworld()
+{
+    cout << "Welcome \nto  \nthe \nmy \nPC "<< a<<endl;
+    cout << "\nn test hello world program by me "<<endl;
+}
+
+int main() 
+{
+    helloworld();
+    return 0;
+}
